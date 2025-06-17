cd468e35a4d14fc2ea62c524636399b65169e33e
KRITI_PORWAL <117333836+prokriti11@users.noreply.github.com>
2025-06-17 16:18:11 +0530

    Time: 0 ms (100%), Space: 41.3 MB (94.43%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        char[] s = palindrome.toCharArray();
+        for(int i=0; i<s.length/2; i++){
+            if(s[i] != 'a'){
+                s[i] = 'a';
+                return new String(s);
+            }
+        }
+
+        s[s.length-1] = 'b';
+        return s.length < 2 ? "" : new String(s); 
+    }
+}
