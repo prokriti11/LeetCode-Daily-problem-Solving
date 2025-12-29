febe3bdf593b4ca3c1c1ce01864c50aad0ec5431
KRITI_PORWAL <117333836+prokriti11@users.noreply.github.com>
2025-12-29 17:31:05 +0530

    Time: 90 ms (73.89%), Space: 49.6 MB (15.29%) - LeetHub

---
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
@@ -0,0 +1,34 @@
+class Solution{
+    Map<String, List<Character>> map = new HashMap<>();
+    Map<String, Boolean> memo = new HashMap<>();
+
+    public boolean pyramidTransition(String bottom, List<String> allowed){
+        for(String s: allowed){
+            String key = s.substring(0,2);
+            map.computeIfAbsent(key, k-> new ArrayList<>()).add(s.charAt(2));
+        }
+        return solve(bottom);
+    }
+    private boolean solve(String bottom){
+        if(bottom.length() == 1) return true;
+        if (memo.containsKey(bottom)) return memo.get(bottom);
+
+        boolean result = generateNextRow(bottom, 0, new StringBuilder());
+        memo.put(bottom, result);
+        return result;
+    }
+    private boolean generateNextRow(String bottom, int idx, StringBuilder currentNext){
+        if(idx == bottom.length() - 1){
+            return solve(currentNext.toString());
+        }
+        String key = bottom.substring(idx, idx + 2);
+        if(!map.containsKey(key)) return false;
+
+        for(char val : map.get(key)){
+            currentNext.append(val);
+            if(generateNextRow(bottom, idx+1, currentNext)) return true;
+            currentNext.deleteCharAt(currentNext.length() - 1);
+        }
+        return false;
+    }
+}
