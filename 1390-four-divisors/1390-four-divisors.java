839262d0a9988cda3628b486a7437279dfd1a001
KRITI_PORWAL <117333836+prokriti11@users.noreply.github.com>
2026-01-04 21:24:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int res= 0;
+
+        for(int num: nums){
+            if(sumOne(num) != -1) res += sumOne(num);
+        }
+        return res;
+    }
+    private int sumOne(int n){
+        int p = (int) Math.round(Math.cbrt(n));
+        if((long) p*p*p == n && isPrime(p)){
+            return 1 + p + p + p + p + p + p;
+        }
+        for(int  i = 2; i*i<=n; i++){
+            if(n%i == 0){
+                int a = i;
+                int b = n/i;
+                if(a != b && isPrime(a) && isPrime(b)){
+                    return 1 + a + b + n;
+                }
+                return -1;
+            }
+        }
+        return -1;
+    }
+    private boolean isPrime(int x){
+        if (x<2) return false;
+        for(int i = 2; i*i <= x; i++){
+            if(x % i == 0) return false;
+        }
+        return true;
+    }
+}
